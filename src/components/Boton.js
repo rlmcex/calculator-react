d81d01990c845e80e1aa79e304d00db3fdc413df
rlmcex <93286613+rlmcex@users.noreply.github.com>
2024-02-24 16:27:51 -0800

    create calculator in raspberry pi 5 using vs code server from proxmox

---
+++ src/components/Boton.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import '../stylesheets/Boton.css';
+
+function Boton(props) {
+
+  const esOperador = valor => {
+    return isNaN(valor) && (valor !== '.') && (valor !== '=');
+  };
+
+
+  if (esOperador(props.children)) {
+    return (
+      <div
+         className='boton-contenedor operador'
+         onClick={() => props.manejarClic(props.children)}>
+          {props.children}
+      </div>
+    );
+  } else {
+    return (
+      <div
+         className='boton-contenedor'
+         onClick={() => props.manejarClic(props.children)}>
+          {props.children}
+      </div>
+
+    );
+  }
+/*  return (
+    <div
+      className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}
+      onClick={() => props.manejarClic(props.children)}>
+      {props.children}
+    </div>
+  );
+
+*/  
+}
+
+export default Boton;
